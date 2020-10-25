a67f252db22cf8706a3be2e3a24c29ee3b9c210f
Artem Shoobovych <shybovycha@gmail.com>
2020-10-25 15:19:43 +1100

    Create a simple 3d vector class to reduce coupling

---
+++ Game/src/Vector3.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <cassert>
+#include <cmath>
+
+template <typename T>
+class Vector3
+{
+public:
+    Vector3();
+
+    Vector3(T x, T y, T z);
+
+    T getX() const;
+
+    T getY() const;
+
+    T getZ() const;
+
+    Vector3<T> setX(T x) const;
+
+    Vector3<T> setY(T y) const;
+
+    Vector3<T> setZ(T z) const;
+
+    Vector3<T> operator+(const Vector3<T> &other) const;
+
+    Vector3<T> operator*(const T value) const;
+
+    Vector3<T> operator/(const T value) const;
+
+    T length() const;
+
+    Vector3<T> normalize() const;
+
+private:
+    T x;
+    T y;
+    T z;
+
+    static T ZERO = { 0 };
+
+    static T ONE = { 1 };
+};
