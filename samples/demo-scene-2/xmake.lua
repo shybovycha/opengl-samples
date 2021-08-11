308f91888dbfac87ea77a55a53b1757bf283040e
Artem Shoobovych <shybovycha@gmail.com>
2021-08-11 20:30:15 +1000

    Start demo scene 2 with new 3D models

--- samples/demo-scene-2/xmake.lua
+++ samples/demo-scene-2/xmake.lua
@@ -4,7 +4,7 @@ add_requires("vcpkg::globjects ~1.1.0", { alias = "globjects" })
 add_requires("vcpkg::glbinding", { alias = "glbinding" })
 add_requires("vcpkg::assimp", { alias = "assimp" })
 
-target("demo-scene-1")
+target("demo-scene-2")
   set_kind("binary")
 
   add_packages("sfml", "glm", "globjects", "glbinding", "assimp")
