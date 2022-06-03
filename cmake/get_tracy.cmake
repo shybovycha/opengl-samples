5fb6ae2e623c1807bbcf7cf21af81ad725ef91e8
shybovycha <shybovycha@gmail.com>
2022-06-04 00:27:05 +1000

    Add tracy only to see memleaks beating the shit out of it

---
+++ cmake/get_tracy.cmake
@@ -0,0 +1,30 @@
+cmake_minimum_required(VERSION 3.20 FATAL_ERROR)
+
+option(TRACY_ENABLE "Enable Tracy" ON)
+
+# set(TRACY_MANUAL_LIFETIME ON)
+set(TRACY_DELAYED_INIT ON)
+set(TRACY_CALLSTACK ON)
+set(TRACY_ON_DEMAND ON)
+
+include(FetchContent)
+
+FetchContent_Declare(
+    tracy
+    GIT_REPOSITORY https://github.com/wolfpld/tracy.git
+    GIT_TAG "v0.8.1"
+    GIT_SHALLOW TRUE
+    GIT_PROGRESS TRUE
+    SOURCE_DIR   "${FETCHCONTENT_BASE_DIR}/tracy/tracy"
+	BINARY_DIR   "${FETCHCONTENT_BASE_DIR}/tracy/bin"
+	SUBBUILD_DIR "${FETCHCONTENT_BASE_DIR}/tracy/subbuild"
+)
+
+FetchContent_MakeAvailable(tracy)
+
+add_library(tracy STATIC "${tracy_SOURCE_DIR}/TracyClient.cpp")
+target_include_directories(tracy PUBLIC "${tracy_SOURCE_DIR}/..")
+
+if (TRACY_ENABLE)
+	target_compile_definitions(tracy PUBLIC "TRACY_ENABLE" "TRACY_DELAYED_INIT" "TRACY_ON_DEMAND")
+endif()
