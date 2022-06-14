a4e27af4e2f702f7aa43223f60bf7708f041f330
Artem Shoobovych <shybovycha@gmail.com>
2022-06-14 22:32:58 +1000

    Start configurable particles app (demo-scene-3)

---
+++ samples/demo-scene-3/xmake.lua
@@ -0,0 +1,29 @@
+add_requires("sfml ~2.5.1", { alias = "sfml" })
+add_requires("glm")
+add_requires("imgui")
+add_requires("vcpkg::globjects", { alias = "globjects" })
+add_requires("vcpkg::glbinding", { alias = "glbinding" })
+add_requires("vcpkg::assimp", { alias = "assimp" })
+
+target("demo-scene-3")
+  set_languages("cxx20")
+  set_kind("binary")
+
+  add_packages("sfml", "glm", "globjects", "glbinding", "assimp", "imgui")
+
+  if is_plat("macosx") then
+    -- this prevents "-[SFOpenGLView enableKeyRepeat]: unrecognized selector sent to instance 0x7fa5c2507970" runtime exception
+    add_ldflags("-ObjC")
+
+    -- this prevents linker errors
+    add_frameworks("Foundation", "OpenGL", "IOKit", "Cocoa", "Carbon")
+  end
+
+  add_files("main.cpp", "ImGuiSfmlBackend.cpp")
+
+  add_defines("HIGH_DPI")
+
+  after_build(function (target)
+    os.cp("$(scriptdir)/../media", path.join(path.directory(target:targetfile()), "media"))
+    os.cp("$(scriptdir)/media", path.join(path.directory(target:targetfile()), "media"))
+  end)
