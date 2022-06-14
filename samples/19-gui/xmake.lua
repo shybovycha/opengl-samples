eef51dd80b8f040f69a305423aaf2f6bfeff8dbf
Artem Shoobovych <shybovycha@gmail.com>
2022-06-14 22:32:28 +1000

    Add imgui to xmake file

--- samples/19-gui/xmake.lua
+++ samples/19-gui/xmake.lua
@@ -1,5 +1,6 @@
 add_requires("sfml ~2.5.1", { alias = "sfml" })
 add_requires("glm")
+add_requires("imgui")
 add_requires("vcpkg::globjects", { alias = "globjects" })
 add_requires("vcpkg::glbinding", { alias = "glbinding" })
 add_requires("vcpkg::assimp", { alias = "assimp" })
@@ -8,7 +9,7 @@ target("19-gui")
   set_languages("cxx20")
   set_kind("binary")
 
-  add_packages("sfml", "glm", "globjects", "glbinding", "assimp")
+  add_packages("sfml", "glm", "globjects", "glbinding", "assimp", "imgui")
 
   if is_plat("macosx") then
     -- this prevents "-[SFOpenGLView enableKeyRepeat]: unrecognized selector sent to instance 0x7fa5c2507970" runtime exception
@@ -18,7 +19,9 @@ target("19-gui")
     add_frameworks("Foundation", "OpenGL", "IOKit", "Cocoa", "Carbon")
   end
 
-  add_files("main.cpp")
+  add_files("main.cpp", "ImGuiSfmlBackend.cpp")
+
+  add_defines("HIGH_DPI")
 
   after_build(function (target)
     os.cp("$(scriptdir)/../media", path.join(path.directory(target:targetfile()), "media"))
