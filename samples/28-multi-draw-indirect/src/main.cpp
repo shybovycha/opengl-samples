52d9b82147ef048d48e341f05049e3cb2a9d88be
shybovycha <shybovycha@gmail.com>
2022-07-05 17:14:17 +1000

    Extract common code in 28-multi-draw-indirect sample

--- samples/28-multi-draw-indirect/src/main.cpp
+++ samples/28-multi-draw-indirect/src/main.cpp
@@ -1,48 +1,4 @@
-#include <filesystem>
-#include <iostream>
-#include <sstream>
-#include <random>
-
-#include <glbinding/gl/gl.h>
-
-#include <globjects/Buffer.h>
-#include <globjects/Error.h>
-#include <globjects/Framebuffer.h>
-#include <globjects/Program.h>
-#include <globjects/Renderbuffer.h>
-#include <globjects/Shader.h>
-#include <globjects/Texture.h>
-#include <globjects/TextureHandle.h>
-#include <globjects/Uniform.h>
-#include <globjects/VertexArray.h>
-#include <globjects/VertexAttributeBinding.h>
-#include <globjects/base/File.h>
-#include <globjects/base/StaticStringSource.h>
-#include <globjects/globjects.h>
-
-#include <glm/ext/matrix_clip_space.hpp>
-#include <glm/ext/matrix_transform.hpp>
-#include <glm/ext/quaternion_float.hpp>
-#include <glm/ext/quaternion_common.hpp>
-#include <glm/ext/quaternion_geometric.hpp>
-#include <glm/ext/quaternion_relational.hpp>
-#include <glm/ext/quaternion_transform.hpp>
-#include <glm/gtx/rotate_vector.hpp>
-#include <glm/mat4x4.hpp>
-#include <glm/vec2.hpp>
-#include <glm/vec3.hpp>
-
-#include <assimp/Importer.hpp>
-#include <assimp/postprocess.h>
-#include <assimp/scene.h>
-
-#include <SFML/Graphics.hpp>
-#include <SFML/OpenGL.hpp>
-#include <SFML/Window.hpp>
-
-#ifdef WIN32
-using namespace gl;
-#endif
+#include "common/stdafx.hpp"
 
 struct StaticGeometryDrawCommand
 {
