66465df99a04b4f6bd7ee7f9f900be0b2e47765a
shybovycha <shybovycha@gmail.com>
2022-07-05 11:33:07 +1000

    Extract common code in 18-reflection sample

--- samples/18-reflection/xmake.lua
+++ samples/18-reflection/xmake.lua
@@ -8,6 +8,8 @@ target("18-reflection")
   set_languages("cxx20")
   set_kind("binary")
 
+  set_pcxxheader("src/common/stdafx.hpp")
+
   add_packages("sfml", "glm", "globjects", "glbinding", "assimp")
 
   if is_plat("macosx") then
@@ -18,7 +20,7 @@ target("18-reflection")
     add_frameworks("Foundation", "OpenGL", "IOKit", "Cocoa", "Carbon")
   end
 
-  add_files("main.cpp")
+  add_files("src/main.cpp", "src/common/AbstractMesh.cpp", "src/common/AbstractMeshBuilder.cpp", "src/common/AbstractSkyboxBuilder.cpp", "src/common/AssimpModel.cpp", "src/common/CubemapSkyboxBuilder.cpp", "src/common/MultimeshModel.cpp", "src/common/SimpleSkyboxBuilder.cpp", "src/common/SingleMeshModel.cpp", "src/common/Skybox.cpp",)
 
   after_build(function (target)
     os.cp("$(scriptdir)/../media", path.join(path.directory(target:targetfile()), "media"))
