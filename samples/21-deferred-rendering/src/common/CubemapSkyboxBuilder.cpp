1bbc05909be86badcc6073da393708d0db012b9f
shybovycha <shybovycha@gmail.com>
2022-07-05 11:46:45 +1000

    Extract common code in 21-deferred-rendering sample

---
+++ samples/21-deferred-rendering/src/common/CubemapSkyboxBuilder.cpp
@@ -0,0 +1,16 @@
+#include "CubemapSkyboxBuilder.hpp"
+
+CubemapSkyboxBuilder::CubemapSkyboxBuilder(std::unique_ptr<globjects::Texture> texture) :
+    m_texture(texture.get()), AbstractSkyboxBuilder()
+{
+}
+
+CubemapSkyboxBuilder::CubemapSkyboxBuilder(globjects::Texture* texture) :
+    m_texture(texture), AbstractSkyboxBuilder()
+{
+}
+
+globjects::Texture* CubemapSkyboxBuilder::getTexture()
+{
+    return m_texture;
+}
